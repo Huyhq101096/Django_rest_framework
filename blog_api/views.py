55a7dc970cbf6bfcde7cdcb122c467db9b2531f6
huy <huy.huynh101096@gmail.com>
2023-06-30 16:59:00 +0700

    update

--- blog_api/views.py
+++ blog_api/views.py
@@ -7,7 +7,9 @@ class PostList(generics.ListCreateAPIView):
     # tra ve all post co du lieu la status = 'published'
     queryset = Post.postobjects.all() 
     serializer_class = PostSerializer
-    pass
+ 
 
 class PostDetail(generics.RetrieveDestroyAPIView):
-    pass
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
